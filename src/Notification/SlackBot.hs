8c08be317cbfdea6bc52ba11567d2316f2f734b3
George Granberry <ggranberry@clearleap.com>
2017-11-29 15:30:11 -0500

    Slackbot up and running

--- src/Notification/SlackBot.hs
+++ src/Notification/SlackBot.hs
@@ -1,56 +1,52 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ExtendedDefaultRules #-}
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE PatternGuards #-}
-{-# OPTIONS_GHC -fno-warn-deprecations #-}
 
-import Data.Maybe
+import Control.Monad.Except (runExceptT)  
+import Control.Exception.Base
+import Control.Monad  
 import Data.Monoid ((<>))
 import qualified Data.Text as T
 import Network.Linklater
 import Network.Wai.Handler.Warp (run)
-import Prelude
 import Utils.Bass as B
 import Scrapers.RobAllen
+import Network.Wreq
 
 readSlackFile :: FilePath -> IO T.Text
 readSlackFile filename = T.filter (/= '\n') . T.pack <$> Prelude.readFile filename
 configIO :: IO Config
 configIO = Config <$> (readSlackFile "hook")
 
--- robAllenResults :: IO Network.Linklater.Message
--- robAllenResults = do
---   basses <- allRABasses
---   return (messageOf [FormatAt user, FormatString (T.intercalate "\n\n". Prelude.map B.toText $ B.liftBasses basses)]) where
---     messageOf = FormattedMessage(EmojiIcon "gift") "bassbot" channel
-
 scrapeCommand :: Command -> IO Network.Linklater.Message
-scrapeCommand (Command "bassbot" user channel (Just text)) = do
+scrapeCommand (Command "scraper" user channel (Just text)) = do
   basses <- allRABasses
-  return (messageOf [FormatAt user, FormatString (T.intercalate "\n\n". Prelude.map B.toText $ B.liftBasses basses)]) where
-    messageOf = FormattedMessage(EmojiIcon "gift") "bassbot" channel
+  return (messageOf [FormatAt user, FormatString (T.intercalate "\n\n". Prelude.map B.toText $ B.liftBasses basses)])
+  where
+    messageOf = FormattedMessage(EmojiIcon ":stars2:") "scraper" channel
+
+showLinkMessage :: Network.Linklater.Message -> T.Text
+showLinkMessage (FormattedMessage a b c _) = T.pack $ (show a) ++ " " ++ (show b) ++ " " ++ (show c)
 
 bassify :: Command -> IO T.Text
 bassify command = do
   Prelude.putStrLn ("+ Incoming command: " <> show command)  
   message <- scrapeCommand command
   config <- configIO
-  Prelude.putStrLn ("+ Outgoing message: " <> show (message))
-  return ""
-  -- case (debug, message) of
-  --   (False,  m) -> do
-  --     _ <- say m config
-  --     return ""
-  --   _ ->
-  --     return ""
-  -- where
-  --   debug = False
+  Prelude.putStrLn ("+ Outgoing message: " <> show (showLinkMessage message))
+  case (debug, message) of
+    (False,  m) -> do
+      anEither <- runExceptT $ say m config
+      case anEither of
+        Left e -> putStrLn ("an error occurred! " <> show e)
+        Right _ -> return ()
+      return ""
+    _ ->
+      return ""
+  where
+    debug = False
 
 main :: IO ()
 main =  do
-	Prelude.putStrLn ("+ Listening on port " <> show port)
-	run port (slashSimple bassify)
-	where 
-		port = 3000
+  Prelude.putStrLn ("+ Listening on port " <> show port)
+  run port (slashSimple bassify)
+  where 
+    port = 3000
